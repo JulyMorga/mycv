79ee724fa8282f2692055310cae8b1a60fc39206
JulianMorganella <julianmorganella@gmail.com>
2020-11-05 22:22:52 -0300

    - Se modifico la forma de funcionar de el enlazamiento a la pagina con el boton de bajar. Arreglando problemas de desplazamientos.

--- js/main.js
+++ js/main.js
@@ -36,15 +36,70 @@ Vue.component("modal-certificado", { // recibe dos argumentos: el nombre del com
 let app = new Vue ({
 	el: "#app",
 	data: {
-		alto: 10,
-		ancho: 10,
+		// alto: 10,
+		// ancho: 10,
+		posicion: 0 ,
+		secciones : ["#section-name", "#about" , "#jobs" , "#education", "#curses","#s-contact"],
+		sec_abajo: "#about",
+		disable_abajo: false,
+		disable_abajo: false,
+		salio_boton: true,
 	},
 	methods: {
-		
+		modificarPosicion : function (pos) {
+			// Seteo la posicion que estoy
+			this.posicion = pos;
+
+			let proxima_pos;
+			// 0 5    proxima_pos =1 
+			// 1 5	   proxima_pos =
+			// 5 5    proxima_pos = 0
+			if(this.posicion > (this.secciones.length - 1)){
+				this.posicion = 0;
+				proxima_pos = 1;
+				this.disable_abajo = false;
+			} else if(this.posicion == (this.secciones.length - 1)){ 
+				proxima_pos = 0;
+				this.disable_abajo = true;
+			} else {
+				proxima_pos = this.posicion + 1;
+				this.disable_abajo = false;
+			}
+			console.log('disable_abajo: '+ this.disable_abajo)
+			this.sec_abajo = this.secciones[proxima_pos];
+		},
+		clickModificarPosicion : function () {
+			if(!this.salio_boton){
+				console.log('posicion: '+ this.posicion)
+				this.posicion++;
+				console.log('posicion: '+ this.posicion)
+			}else {
+				this.salio_boton = false;
+			}
+			//pos_act max pos		prox
+			// 0  		5    1		proxima_pos = 2 
+			// 1  		5	 2 		proxima_pos = 3
+			// 5  		5    0		proxima_pos = 1
+
+			// if(this.posicion >= (this.secciones.length-1)){
+			// 	this.posicion = 0;
+			// }
+			this.modificarPosicion(this.posicion);
+		}
 	},
 	computed: { // computed para las funciones que retornen algo
+		// bajarAPosicion: function() {
+		// 	console.log('pos ant:' + this.posicion)
+		// 	this.posicion++;
+		// 	console.log('pos act:' + this.posicion)
+		// 	if(this.posicion > this.secciones.length){
+		// 		this.posicion = 0;
+		// 	}
+		// 	sec_abajo = this.secciones[this.posicion+1];
+		// 	return this.secciones[this.posicion+1];
+		// },
 		
-	}
+	},
 })
 
 
@@ -54,35 +109,35 @@ let sec_job = document.querySelector("#jobs");
 let sec_edu = document.querySelector("#education");
 let sec_curses = document.querySelector("#curses");
 let sec_cont = document.querySelector("#s-contact");
-let btn_bajar = document.querySelector("#btn_bajar");
+// let btn_bajar = document.querySelector("#btn_bajar");
 
-alturas = [sec_name.scrollHeight, sec_job.scrollHeight , sec_edu.scrollHeight , sec_curses.scrollHeight ,sec_cont.scrollHeight]
+// alturas = [sec_name.scrollHeight, sec_job.scrollHeight , sec_edu.scrollHeight , sec_curses.scrollHeight ,sec_cont.scrollHeight]
+// seccioness = ["#section-name", "#about" , "#jobs" , "#education", "#curses","#s-contact"]
 
+// var position = 0;
+// var modificarPosicion = function (pos) {
+// 	position = pos;
+// }
 
-var position = 0;
-var modificarPosicion = function (pos) {
-	position = pos;
-}
+// var bajarPosicion = function () {
+// 	alt = 0;
+// 	alturas = [sec_name.scrollHeight, sec_job.scrollHeight , sec_edu.scrollHeight , sec_curses.scrollHeight ,sec_cont.scrollHeight]
+// 	for (let i = 0; i < (position+1); i++) {
+// 		alt += alturas[i];
+// 	}
+// 	position++;
+// 	if(position > alturas.length){
+// 		position = 0;
+// 		alt = 0;
+// 	}
 
-var bajarPosicion = function () {
-	alt = 0;
-	alturas = [sec_name.scrollHeight, sec_job.scrollHeight , sec_edu.scrollHeight , sec_curses.scrollHeight ,sec_cont.scrollHeight]
-	for (let i = 0; i < (position+1); i++) {
-		alt += alturas[i];
-	}
-	position++;
-	if(position > alturas.length){
-		position = 0;
-		alt = 0;
-	}
+// 	scrollTo(0,alt)
+// }
 
-	scrollTo(0,alt)
-}
-
-sec_name.addEventListener('mouseenter', function(){modificarPosicion(0)});
-sec_about.addEventListener('mouseenter', function(){modificarPosicion(1)});
-sec_job.addEventListener('mouseenter', function(){modificarPosicion(2)});
-sec_edu.addEventListener('mouseenter', function(){modificarPosicion(3)});
-sec_curses.addEventListener('mouseenter', function(){modificarPosicion(4)});
-sec_cont.addEventListener('mouseenter', function(){modificarPosicion(5)});
-btn_bajar.addEventListener('click', bajarPosicion);
+// sec_name.addEventListener('mouseenter', function(){modificarPosicion(0)});
+// sec_about.addEventListener('mouseenter', function(){modificarPosicion(1)});
+// sec_job.addEventListener('mouseenter', function(){modificarPosicion(2)});
+// sec_edu.addEventListener('mouseenter', function(){modificarPosicion(3)});
+// sec_curses.addEventListener('mouseenter', function(){modificarPosicion(4)});
+// sec_cont.addEventListener('mouseenter', function(){modificarPosicion(5)});
+// btn_bajar.addEventListener('click', bajarPosicion);
